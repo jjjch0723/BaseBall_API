765a4dcfa171a5ca79b4ba5afd0e44e002efe68f
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-06-03 10:00:59 +0900

    LLMAM 테스트용 데이터

--- py/Crowling_Data_py/get_MLB_member.py
+++ py/Crowling_Data_py/get_MLB_member.py
@@ -1,11 +1,9 @@
-## MLB 선수 테이블
-
 import statsapi
 import json
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def get_player_info(player_id, team_id, team_name):
+def get_player_info(player_id, team_id, team_name, league_name):
     player_info = statsapi.player_stat_data(player_id)
     return {
         'fullName': player_info['first_name'] + ' ' + player_info['last_name'],
@@ -14,14 +12,15 @@ def get_player_info(player_id, team_id, team_name):
         'position': player_info['position'],
         'teamCode': team_id,
         'teamName': team_name,
+        'leagueName': league_name,
         'playerId': player_id
     }
 
-def get_team_roster(team_id, team_name):
+def get_team_roster(team_id, team_name, league_name):
     roster = statsapi.get('team_roster', {'teamId': team_id})['roster']
     player_data = []
     with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(get_player_info, player['person']['id'], team_id, team_name) for player in roster]
+        futures = [executor.submit(get_player_info, player['person']['id'], team_id, team_name, league_name) for player in roster]
         for future in as_completed(futures):
             player_data.append(future.result())
     return player_data
@@ -31,7 +30,7 @@ def get_all_players():
     all_players = []
 
     with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(get_team_roster, team['id'], team['name']) for team in teams]
+        futures = [executor.submit(get_team_roster, team['id'], team['name'], team['league']['name']) for team in teams]
         for future in as_completed(futures):
             all_players.extend(future.result())
 
